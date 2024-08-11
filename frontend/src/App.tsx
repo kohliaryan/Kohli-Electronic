e6d7508e6952bcab21cfbf103b0868b233902549
kohliaryan <kohliaryan2004@gmail.com>
2024-08-11 18:19:45 +0530

    Renaming client to Frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import { Navbar } from "./components/Navbar";
+
+function App() {
+
+  return (
+    <BrowserRouter>
+    <Navbar></Navbar>
+    </BrowserRouter>
+  );
+}
+
+export default App;
