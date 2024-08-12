57f0d8dd6325c2881078b80b14289977c42400a2
kohliaryan <kohliaryan2004@gmail.com>
2024-08-12 13:58:19 +0530

    Adding create category route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,13 @@
 import { Hono } from 'hono'
+import { categoryRoute } from './v1/category'
 
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('Welcome to Kohli Electronics')
 })
+
+app.route('/v1/admin', categoryRoute)
+
 
 export default app
